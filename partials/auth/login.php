4285d860132134fbe000a54e2217c5eacbf85c33
Lobo Linarez <lalinaresr@hotmail.com>
2024-04-09 01:32:48 -0600

    Fixing the classes for the horizontal form in the login

--- partials/auth/login.php
+++ partials/auth/login.php
@@ -2,25 +2,25 @@
     <div class="panel panel-default">
         <div class="panel-heading">Identificarse</div>
         <div class="panel-body">
-            <div class="row">
-                <form id="form-login" method="POST">
-                    <div class="form-group">
-                        <label for="email" class="col-md-3 mb-3">E-mail:</label>
-                        <div class="col-md-9 mb-3">
-                            <input type="email" id="email" name="email" value="sferamgmx@gmail.com" class="form-control" placeholder="Ingrese su correo electrónico" required autocomplete="off" autofocus="true">
-                        </div>
+            <form id="form-login" method="POST" class="form-horizontal">
+                <div class="form-group">
+                    <label for="email" class="col-md-3 control-label">E-mail:</label>
+                    <div class="col-md-9">
+                        <input type="email" id="email" name="email" value="sferamgmx@gmail.com" class="form-control" placeholder="Ingrese su correo electrónico" required autocomplete="off" autofocus="true">
                     </div>
-                    <div class="form-group">
-                        <label for="password" class="col-md-3 mb-3">Contraseña:</label>
-                        <div class="col-md-9 mb-3">
-                            <input type="password" id="password" name="password" value="password" class="form-control" placeholder="Ingrese su contraseña" required>
-                        </div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-md-3 control-label">Contraseña:</label>
+                    <div class="col-md-9">
+                        <input type="password" id="password" name="password" value="password" class="form-control" placeholder="Ingrese su contraseña" required>
                     </div>
+                </div>
+                <div class="form-group">
                     <div class="col-md-9 col-md-offset-3">
                         <button type="submit" id="btn-login" class="btn btn-default"><span class="glyphicon glyphicon-lock"></span> Entrar</button>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
